061138b581b335cf2d12c2c130a885f27c82a540
Ryota Hagi <hagi.ryota@plus-zero.co.jp>
2025-09-25 13:10:45 +0900

    feat process_data2

--- src/logic/pipeline.py
+++ src/logic/pipeline.py
@@ -3,5 +3,10 @@ def process_data():
     print("Processing data...")
     return
 
+def process_data2():
+    print("Processing data 2...")
+    return
+
 if __name__ == "__main__":
     process_data()
+    process_data2()
